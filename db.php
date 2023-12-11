6e841c10ee412164c0a9e8a47e9fc909f79d87d0
PameSegovia <114256835+PameSegovia@users.noreply.github.com>
2023-12-10 23:33:19 -0300

    Leer datos de mysql

--- db.php
+++ db.php
@@ -9,10 +9,18 @@ try{
 	$conexion=new PDO("mysql:host=$servidor;dbname=album", $usuario, $contrasenia );
 	$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
-	$sql="INSERT INTO `fotos` (`id`, `nombre`, `ruta`) VALUES (NULL, 'Jugando con la Programación', 'foto.jpg');";
+	$sql="SELECT * FROM `fotos`";
 
+	$sentencia=$conexion->prepare($sql);
+	$sentencia->execute();
+
+	$resultado=$sentencia->fetchAll();
+
+	//print_r($resultado);
 	
-	
+	foreach($resultado as $foto){
+		echo $foto['nombre']."<br/>";
+	}
 	echo "Conexión Establecida";
 
 }catch(PDOException $error){
