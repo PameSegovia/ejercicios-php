5ef29c6bde1b84be16146cc39f27abcbc397b6c6
PameSegovia <114256835+PameSegovia@users.noreply.github.com>
2023-12-10 23:01:23 -0300

    Base de Datos

--- base-de-datos.php
+++ base-de-datos.php
@@ -8,6 +8,11 @@ try{
 
 	$conexion=new PDO("mysql:host=$servidor;dbname=album", $usuario, $contrasenia );
 	$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+	$sql="INSERT INTO `fotos` (`id`, `nombre`, `ruta`) VALUES (NULL, 'Jugando con la Programación', 'foto.jpg');";
+
+	$conexion->exec($sql);
+	
 	echo "Conexión Establecida";
 
 }catch(PDOException $error){
